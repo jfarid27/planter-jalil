7c35d555404f4e459e6bbf8b244f4cc1a6dfed07
J. A. Farid <jrab227@gmail.com>
2018-11-11 22:05:18 -0500

    Fixing

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const util = require('./util');
 const switches = _.forEach(config.switches, util.genSwitch);
 const reads = _.forEach(config.reads, util.genRead);
 
-const runMain = (switches, readIds) => () => 
-  util.checkGpio()
+const runMain = () => util.checkGpio()
     .then(() => Promise.all(switches, util.turnOnSwitch))
-    .then(() => util.testSignal(reads))
+    .then(() => Promise.all(reads, util.testSignal))
     .catch(util.handleError(config.error));
 
-setInterval(runMain(switches, reads), config.interval);
+setInterval(runMain, config.interval);
