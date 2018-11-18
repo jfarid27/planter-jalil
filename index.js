9108f3af706f0226353215b56b201b4cdf64fef7
J. A. Farid <jrab227@gmail.com>
2018-11-18 02:27:18 -0500

    F

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const reads = _.map(config.reads, util.genRead(config.data));
 async function runMain() {
   try {
     await util.checkGpio()
-    await Promise.all(switches, util.setSwitch(1));
-    await Promise.all(reads, util.testSignal);
-    await Promise.all(switches, util.setSwitch(0));
+    await Promise.all(_.map(switches, util.setSwitch(1)));
+    await Promise.all(_.map(reads, util.testSignal));
+    await Promise.all(_.map(switches, util.setSwitch(0)));
   } catch (err) {
     util.handleError(config.error, err);
   }
